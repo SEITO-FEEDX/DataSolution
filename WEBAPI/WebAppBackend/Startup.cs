ae3fabb8cc21b34fdb8532f0ef11aefad5e2522b
ahmed errih <Ahmed.Errih.Dev@gmail.com>
2021-02-25 16:57:37 +0100

    my project

---
+++ WEBAPI/WebAppBackend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppBackend.Startup))]
+namespace WebAppBackend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
